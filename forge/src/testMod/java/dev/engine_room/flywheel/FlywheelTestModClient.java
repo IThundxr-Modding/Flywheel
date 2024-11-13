e35955873bb7a211d4fe07dcffda539f20a12507
IThundxr <me@ithundxr.dev>
2024-11-13 10:11:30 -0500

    Update test mod for neoforge

--- forge/src/testMod/java/dev/engine_room/flywheel/FlywheelTestModClient.java
+++ forge/src/testMod/java/dev/engine_room/flywheel/FlywheelTestModClient.java
@@ -1,14 +1,14 @@
 package dev.engine_room.flywheel;
 
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLLoader;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.loading.FMLLoader;
 
 @Mod("flywheel_testmod")
 public class FlywheelTestModClient {
@@ -18,16 +18,14 @@ public class FlywheelTestModClient {
 	public FlywheelTestModClient() {
 		LOGGER.info("Starting {} on Dist: {}", NAME, FMLLoader.getDist());
 
-		MinecraftForge.EVENT_BUS.addListener((TickEvent.ClientTickEvent e) -> {
-			if (e.phase == TickEvent.Phase.END) {
-				LOGGER.info("Running mixin audit");
-				MixinEnvironment.getCurrentEnvironment()
-						.audit();
+		NeoForge.EVENT_BUS.addListener((ClientTickEvent.Post e) -> {
+			LOGGER.info("Running mixin audit");
+			MixinEnvironment.getCurrentEnvironment()
+					.audit();
 
-				LOGGER.info("Stopping client");
-				Minecraft.getInstance()
-						.stop();
-			}
+			LOGGER.info("Stopping client");
+			Minecraft.getInstance()
+					.stop();
 		});
 	}
 }
